b9ac64e98832b6262e6908621371d9fc7c18c071
Alberto Amores <AlbertoAmnez>
2024-02-14 17:37:51 +0100

    añadir metodo para dia de la semana en forma de texto y alguna correcion

--- calendar.py
+++ calendar.py
@@ -14,7 +14,7 @@ class Dia:
             raise ValueError("El mes tiene que estar entre 1 y 12")
         
         if self.dia < 1 or self.dia > 31:
-            raise ValueError("El día tiene estar entre 1 y 31")
+            raise ValueError("El dia tiene estar entre 1 y 31")
         '''
 
         Inicializamos la lista dias_por_mes con '0' para que coincidan los indices de la lista con los numeros de mes, ya que el indice empieza en 0 pero el mes empieza en 1.
@@ -46,22 +46,30 @@ class Dia:
     
     def info(self):
         return f"{self.dia:02d}/{self.mes:02d}/{self.anyo}"
+    
+'''
+Creo este metodo que retorca la cadena de texto correspondiente a la posicion del dia de la semana, por lo tanto si la posicion es [0] me devulve: "Sabado"
+'''
 
+    def dia_semana_texto(self):
+        dias_texto = ["Sabado", "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+        return dias_texto[self.dia_semana]
 
 try:
     d = Dia(1970, 4, 25)
     print(d.info()) #Imprime la fecha si se cumplen las condiciones
+    print(d.dia_semana_texto())
 except ValueError as date:
     print(date) 
 
 '''
 
-0 = sábado, 1 = domingo, 2 = lunes, 3 = martes, 4 = miercoles, 5 = jueves, 6 = viernes
+0 = sabado, 1 = domingo, 2 = lunes, 3 = martes, 4 = miercoles, 5 = jueves, 6 = viernes
 
 '''
 
 try:
-    d = Dia(1970, 4, 8)
-    print(f"Día de la semana: {d.dia_semana}")  
+    d = Dia(1970, 4, 25)
+    print(f"Dia de la semana: {d.dia_semana}")  
 except ValueError as date:
     print(date)
